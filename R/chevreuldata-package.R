7a9822cf13cc0ac9338383e219e26d56d40a9ad4
whtns <kevin.stachelek@gmail.com>
2024-02-23 16:59:15 -0800

    update namespace

--- R/chevreuldata-package.R
+++
@@ -1,9 +0,0 @@
-#' Title
-#'
-#' @description
-#'
-#' @name chevreuldata-package
-#' @aliases chevreuldata-package chevreuldata
-#' @docType package
-#' @author
-NULL
--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,5 @@
+#' @importFrom ExperimentHub createHubAccessors
+#' @importFrom utils read.csv
 .onLoad <- function(libname, pkgname) {
     fl <- system.file("extdata", "metadata.csv", package = pkgname)
     titles <- read.csv(fl, stringsAsFactors = FALSE)$Title
