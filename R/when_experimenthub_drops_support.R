edd64da6c8d003af95d60ad2f61eda74b5102324
whtns <kevin.stachelek@gmail.com>
2024-03-24 20:50:32 -0700

    when experimenthub drops support

---
+++ R/when_experimenthub_drops_support.R
@@ -0,0 +1,8 @@
+human_gene_transcript_sce <- function(){
+	loadRData <- function(fileName) {
+		# loads an RData file, and returns it
+		load(fileName)
+		get(ls()[ls() != "fileName"])
+	}
+	loadRData(url("http://cobrinik-1.saban-chla.usc.edu/chevreuldata/human_gene_transcript_sce.rda"))
+}
