667abf5eee49495b933d6f1c3c18b7ba0168e77d
whtns <kevin.stachelek@gmail.com>
2024-08-05 11:26:52 -0700

    explicit reference to accessor

--- R/zzz.R
+++ R/zzz.R
@@ -6,3 +6,13 @@
     ExperimentHub::createHubAccessors(pkgname, titles)
 
 }
+
+#' human_gene_transcript_sce
+#'
+#' Human retinal organoid data from
+#' https://www.biorxiv.org/content/10.1101/2023.02.28.530247v1
+#'
+#' @name human_gene_transcript_sce
+#' @docType data
+#' @author Kevin Stachelek \email{kevin.stachelek@gmail.com}
+NULL
