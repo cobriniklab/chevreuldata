461085a417774b1a230f4fe90a74c3324212b845
whtns <kevin.stachelek@gmail.com>
2024-10-14 15:00:25 -0700

    add human fetal retina dataset

--- R/data.R
+++ R/data.R
@@ -17,3 +17,23 @@
 #' human_gene_transcript_sce()
 #'
 NULL
+
+#' shayler_developing_human_retina_sce
+#'
+#' Human fetal retina data from
+#' https://www.biorxiv.org/content/10.1101/2023.02.28.530247v1
+#'
+#' @format a SingleCellExperiment object
+#' @source https://www.biorxiv.org/content/10.1101/2023.02.28.530247v1
+#' @name shayler_developing_human_retina_sce
+#' @docType data
+#' @keywords datasets
+#' @usage shayler_developing_human_retina_sce(metadata=FALSE)
+#' @param metadata \code{logical} value indicating whether only
+#'     metadata should be returned or if the resource
+#'     should be loaded
+#' @return a SingleCellExperiment object
+#' @examples
+#' shayler_developing_human_retina_sce()
+#'
+NULL
--- R/globals.R
+++ R/globals.R
@@ -1,1 +1,1 @@
-utils::globalVariables(c("human_gene_transcript_sce"))
+utils::globalVariables(c("human_gene_transcript_sce", "shayler_developing_human_retina_sce"))
