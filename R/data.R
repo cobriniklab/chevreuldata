c639116a41ce514383db3cdf5045f21be1c7baf7
whtns <kevin.stachelek@gmail.com>
2024-11-18 11:35:55 -0800

    trigger rebuild

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' human_gene_transcript_sce
 #'
-#' Human retinal organoid data from
+#' Human fetal retina data from
 #' https://www.biorxiv.org/content/10.1101/2023.02.28.530247v1
 #'
 #' @format a SingleCellExperiment object
@@ -15,25 +15,5 @@
 #' @return a SingleCellExperiment object
 #' @examples
 #' human_gene_transcript_sce()
-#'
-NULL
-
-#' shayler_developing_human_retina_sce
-#'
-#' Human fetal retina data from
-#' https://www.biorxiv.org/content/10.1101/2023.02.28.530247v1
-#'
-#' @format a SingleCellExperiment object
-#' @source https://www.biorxiv.org/content/10.1101/2023.02.28.530247v1
-#' @name shayler_developing_human_retina_sce
-#' @docType data
-#' @keywords datasets
-#' @usage shayler_developing_human_retina_sce(metadata=FALSE)
-#' @param metadata \code{logical} value indicating whether only
-#'     metadata should be returned or if the resource
-#'     should be loaded
-#' @return a SingleCellExperiment object
-#' @examples
-#' shayler_developing_human_retina_sce()
 #'
 NULL
